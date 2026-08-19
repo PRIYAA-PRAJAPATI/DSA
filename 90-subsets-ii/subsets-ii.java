7af02838544929a2b6b5ccd0d374e271895bf80a
PRIYAA-PRAJAPATI <priyapahadwal@gmail.com>
2026-08-19 16:06:20 +0530

    Time: 3 ms (54.59%) | Memory: 43.6 MB (99.97%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -1,59 +1,38 @@
 class Solution {
 
-    void fun(List<Integer> nums,
-             List<Integer> tmp,
-             List<List<Integer>> ans) {
+    List<List<Integer>> ans = new ArrayList<>();
 
-        ans.add(new ArrayList<>(tmp));
+    void solve(int[] nums, int start, List<Integer> cur) {
 
-        if (nums.size() == 0) {
-            return;
-        }
+        
+        ans.add(new ArrayList<>(cur));
 
-        for (int i = 0; i < nums.size(); i++) {
+        for (int i = start; i < nums.length; i++) {
 
-            List<Integer> ip = new ArrayList<>(nums);
-            List<Integer> op = new ArrayList<>(tmp);
-
-            if (i > 0 && nums.get(i).equals(nums.get(i - 1))) {
+            
+            if (i > start && nums[i] == nums[i - 1]) {
                 continue;
             }
 
-            op.add(nums.get(i));
+            
+            cur.add(nums[i]);
 
-            for (int j = 0; j <= i; j++) {
-                ip.remove(0);
-            }
+            
+            solve(nums, i + 1, cur);
 
-            fun(ip, op, ans);
+            
+            cur.remove(cur.size() - 1);
         }
     }
 
     public List<List<Integer>> subsetsWithDup(int[] nums) {
 
-        List<Integer> tmp = new ArrayList<>();
-        List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> input = new ArrayList<>();
-
+        
         Arrays.sort(nums);
 
-        for (int x : nums) {
-            input.add(x);
-        }
+        List<Integer> cur = new ArrayList<>();
 
-        fun(input, tmp, ans);
-
-        Collections.sort(ans, (a, b) -> {
-            int n = Math.min(a.size(), b.size());
-
-            for (int i = 0; i < n; i++) {
-                if (!a.get(i).equals(b.get(i))) {
-                    return a.get(i) - b.get(i);
-                }
-            }
-
-            return a.size() - b.size();
-        });
+        solve(nums, 0, cur);
 
         return ans;
     }
