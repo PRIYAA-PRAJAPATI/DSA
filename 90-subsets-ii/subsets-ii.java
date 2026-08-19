729a1dd796b48748451c53b92161c66049493f74
PRIYAA-PRAJAPATI <priyapahadwal@gmail.com>
2026-08-19 15:32:05 +0530

    Time: 4 ms (16.53%) | Memory: 45.2 MB (42.50%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -2,17 +2,9 @@ class Solution {
 
     void fun(List<Integer> nums,
              List<Integer> tmp,
-             List<List<Integer>> ans,
-             Map<List<Integer>, Integer> m1) {
-
-        Collections.sort(tmp);
-
-        if (m1.containsKey(tmp)) {
-            return;
-        }
+             List<List<Integer>> ans) {
 
         ans.add(new ArrayList<>(tmp));
-        m1.put(new ArrayList<>(tmp), 1);
 
         if (nums.size() == 0) {
             return;
@@ -23,13 +15,17 @@ class Solution {
             List<Integer> ip = new ArrayList<>(nums);
             List<Integer> op = new ArrayList<>(tmp);
 
+            if (i > 0 && nums.get(i).equals(nums.get(i - 1))) {
+                continue;
+            }
+
             op.add(nums.get(i));
 
             for (int j = 0; j <= i; j++) {
                 ip.remove(0);
             }
 
-            fun(ip, op, ans, m1);
+            fun(ip, op, ans);
         }
     }
 
@@ -37,15 +33,15 @@ class Solution {
 
         List<Integer> tmp = new ArrayList<>();
         List<List<Integer>> ans = new ArrayList<>();
-        Map<List<Integer>, Integer> m1 = new HashMap<>();
-
         List<Integer> input = new ArrayList<>();
+
+        Arrays.sort(nums);
 
         for (int x : nums) {
             input.add(x);
         }
 
-        fun(input, tmp, ans, m1);
+        fun(input, tmp, ans);
 
         Collections.sort(ans, (a, b) -> {
             int n = Math.min(a.size(), b.size());
