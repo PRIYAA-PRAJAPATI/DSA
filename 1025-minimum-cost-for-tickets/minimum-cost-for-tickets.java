ce10fe6a8609e576c6a9f9003ddea5df15886a97
PRIYAA-PRAJAPATI <priyapahadwal@gmail.com>
2026-09-11 09:14:25 +0530

    Time: 1 ms (69.27%) | Memory: 43.6 MB (18.23%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.java
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.java
@@ -1,39 +1,36 @@
 class Solution {
     int[] dp = new int[1000];
-    int fun(int i,int[] nums,int[]cost){
-        if(i>=nums.length)
+    int fun(int i,int[] days,int[]cost){
+        if(i>=days.length)
         return 0;
         if(dp[i] != -1)
         return dp[i];
          
-         int c1 = cost[0] + fun(i+1,nums,cost); //day1 pass
+         int one = cost[0] + fun(i+1,days,cost); //day1 pass
 
-         int id1 = nums.length;
+         int id1 = days.length;
          
-         int id2 = nums.length;
+         int id2 = days.length;
           
-          int l = i+1; //to find 1st day >=nums[i]+7 by binary 
-          int h = nums.length - 1;
-          while(l<=h){
-            int mid = (l+h)/2;
-            if(nums[mid]>=nums[i]+7){
-                id1 = mid;
-                h=mid-1;
-            }else{
-                l=mid+1;
+         for(int j = i+1;j<days.length;j++){
+            if(days[j]>=days[i]+7){
+                id1 = j;
+                break;
             }
-          }
-          for(int j = i+1;j<nums.length;j++){ //to find 1st day >=nums[i]+30
-            if(nums[j] >= nums[i]+30){
+         }
+            
+          
+          for(int j = i+1;j<days.length;j++){ //to find 1st day >=nums[i]+30
+            if(days[j] >= days[i]+30){
                 id2 = j;
                 break;
             }
           
 
     }
-    int c2 = cost[1]+fun(id1,nums,cost); //7day pass
-    int c3 = cost[2]+fun(id2,nums,cost); //30 
-    return dp[i]=Math.min(c1,Math.min(c2,c3)); //min
+    int seven = cost[1]+fun(id1,days,cost); //7day pass
+    int thirty = cost[2]+fun(id2,days,cost); //30 
+    return dp[i]=Math.min(one,Math.min(seven,thirty)); //min
 }
     public int mincostTickets(int[] days, int[] costs) {
         Arrays.fill(dp,-1);
